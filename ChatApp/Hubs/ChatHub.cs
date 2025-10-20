db7c25e7b61220bbbbc5395294bd568a7aef1a56
Guilherme <guiseredko@gmail.com>
2025-10-20 12:45:03 -0300

    Adicionar arquivos de projeto.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage (string user, string message)
+        {
+            // envia mensagem para todos que estão conectados
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ ChatApp/wwwroot/Scripts/Index.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    // Criar conexão com o Hub SiganlR
+
+    let username = "";
+
+    $('#usernameModal').modal('show');
+
+    $('#confirmUsername').click(function () {
+        const inputName = $('#modalUsername').val().trim();
+
+        if (inputName) {
+            username = inputName;
+            $('#usernameModal').modal('hide');
+            startChar(username);
+        } else {
+            alert("Por favor, insira um nome");
+        }
+
+    });
+
+
+    function startChar(user) {
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("/chatHub")
+            .build();
+
+        //Quando servidor enviar mensagem
+        connection.on("ReceiveMessage", function (user, message) {
+            const $li = $("<li>").text(`${user}: ${message}`);
+            $("#messagesList").append($li);
+        });
+
+        //Inicio conexão
+        connection.start().catch(function (err) {
+            console.error(err.toString());
+        });
+
+        $("#sendButton").click(function () {
+            const message = $("#messageInput").val().trim();
+
+            if (user && message) {
+                connection.invoke("SendMessage", user, message)
+                    .catch(function (err) {
+                        console.error(err.toString());
+                    });
+                $("messageInput").val("")
+            }
+        });
+    }
+});
